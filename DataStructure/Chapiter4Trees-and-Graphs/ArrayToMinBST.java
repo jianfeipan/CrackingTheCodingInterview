1eca0e4fb2c5060fd0adcb3856e268d77f617375
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-07 10:14:56 +0100

    Update ArrayToMinBST.java

--- DataStructure/Chapiter4Trees-and-Graphs/ArrayToMinBST.java
+++ DataStructure/Chapiter4Trees-and-Graphs/ArrayToMinBST.java
@@ -1,6 +1,14 @@
 /*Given a sorted (increasing order) array, write an algorithm to create a binary tree with minimal height.*/
 public class ArrayToMinBST{
-  public static addTreeNode(int[] array,int start, int end){
-    
+  public static TreeNode addTreeNode(int[] array,int start, int end){
+    int mid=(start+end)/2;
+    TreeNode node = new TreeNode(array[mid]);
+    node.left=addTreeNode(array,start,mid-1);
+    node.right=addTreeNode(array,mid+1,end);
+    return node;
+  }
+  
+  public static TreeNode createMinimalBST(int[] array){
+    return addTreeNode(array, 0, array.lenght-1);
   }
 }
