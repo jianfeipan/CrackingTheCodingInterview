c13518af83f693da2873bd9ce9a6edf68768e4fd
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-07 18:02:23 +0100

    Create InorderSucc.java

---
+++ DataStructure/Chapiter4Trees-and-Graphs/InorderSucc.java
@@ -0,0 +1,4 @@
+/*Write an algorithm to find the ‘next’ node (e.g., in-order successor) of a given node in a binary search tree where each node has a link to its parent.*/
+public class InorderSucc{
+    
+}
