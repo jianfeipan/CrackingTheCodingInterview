75a55e7e00854f7aaa94872918e12178ced3ac53
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-08 22:50:15 +0100

    Create SubTree.java

---
+++ DataStructure/Chapiter4Trees-and-Graphs/SubTree.java
@@ -0,0 +1,4 @@
+/*You  have  two  very  large  binary  trees: T1,with  millions  of  nodes,  and T2,  with  hun-dreds of nodes   Create an algorithm to decide if T2 is a subtree of T1*/
+public class SubTree{
+    
+}
