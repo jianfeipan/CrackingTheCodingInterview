9d218f6a75b2871e9507bfcb69d2f4e15338ad70
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-08 22:47:32 +0100

    Update CommenAncestor.java

--- DataStructure/Chapiter4Trees-and-Graphs/CommenAncestor.java
+++ DataStructure/Chapiter4Trees-and-Graphs/CommenAncestor.java
@@ -2,7 +2,20 @@
 in a binary  tree   Avoid storing additional nodes in a data structure    NOTE: This is not 
 necessarily a binary search tree  */
 public class CommenAncestor{
-    public static TreeNode findCommenAncestor(TreeNode noodeA, TreeNode noodB){
-         
+    public static TreeNode commonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if(covers(root.left,p)&&covers(root.left,q))
+            return commenAncestor(root.left,p,q);
+        if(covers(root.right,p)&&covers(root.right,q))
+            return commenAncestor(root.right,p,q);
+        return root;
+    }
+    
+    /*
+    if root is a ancestor of root
+    */
+    public static boolean covers(TreeNode root, TreeNode node){
+        if(root==null) return false;
+        if(root==p) return true;
+        return covers(root.left,node)||covers(root.right,node);
     }
 }
