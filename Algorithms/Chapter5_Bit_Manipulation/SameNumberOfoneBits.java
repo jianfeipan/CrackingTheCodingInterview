8b983091f5161c03c65ded997da60ece52e8029b
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-09 11:54:35 +0100

    Update SameNumberOfoneBits.java

--- Algorithms/Chapter5_Bit_Manipulation/SameNumberOfoneBits.java
+++ Algorithms/Chapter5_Bit_Manipulation/SameNumberOfoneBits.java
@@ -6,12 +6,26 @@ public class SameNumberOfoneBits{
     int[] result=new int[2];
     int test=1;
     int n=0;
+    result[0]=input;
+    boolean first1=false;
+    boolean first0=false;
     for(int i=0; i<=31;i++){
-      if((input&(test<<i))==1)
+      if((input&(test<<i))==1){
         n++;
+        if(first1==false){
+          result[0]=result[0]&(~(test<<i));//turn off the lowest 1 to 0
+          first1=true;
+        }
+      }else{
+        if(first0==false){
+          result[0]=result[0]|(test<<i);//turn on the lowest 0 to 1
+          first0=true;
+        }
+      }
+      
+
     }
-    result[0]=(~0)>>(31-n);
-    result[1]=(~0)<<(31-n);
+    result[1]=(~0)<<(32-n);
     return result;
   }
 }
