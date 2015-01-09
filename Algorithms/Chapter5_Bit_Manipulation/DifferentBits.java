fd479b87606c3394bd84ea87136c061f2d3d3c3c
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-09 12:10:27 +0100

    Update DifferentBits.java

--- Algorithms/Chapter5_Bit_Manipulation/DifferentBits.java
+++ Algorithms/Chapter5_Bit_Manipulation/DifferentBits.java
@@ -2,4 +2,11 @@
 Input: 31, 14
 Output: 2*/
 public class DifferentBits{
+  public static int numberOfDifferentbits(int a, int b){
+    int count=0;
+    int c=a^b;
+    for(c;c!=0;c=c>>1){
+      count+=c&1;
+    } 
+    return count;
 }
