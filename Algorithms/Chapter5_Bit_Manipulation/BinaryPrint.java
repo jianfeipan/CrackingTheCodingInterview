59206ce91a98defdf94ca12b1dfe481dd11b996d
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-09 10:39:30 +0100

    Create BinaryPrint.java

---
+++ Algorithms/Chapter5_Bit_Manipulation/BinaryPrint.java
@@ -0,0 +1,5 @@
+/*
+Given a (decimal - e.g. 3.72) number that is passed in as a string, print the binary representation. If the number can not be represented accurately in binary, print “ERROR”
+*/
+public class BinaryPrint{
+}
