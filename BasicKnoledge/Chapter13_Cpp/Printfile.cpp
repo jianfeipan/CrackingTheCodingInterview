0d4a1b782959b3af9d9e900483f5f576c12c15c9
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-12 16:10:59 +0100

    Update Printfile.cpp

--- BasicKnoledge/Chapter13_Cpp/Printfile.cpp
+++ BasicKnoledge/Chapter13_Cpp/Printfile.cpp
@@ -2,3 +2,20 @@
 Write a method to print the last K lines of an input file using C++.
 */
 string L[K];
+int lines=0;
+while(file.good()){
+  getline(file,L[lines%K]);
+  ++lines;
+}
+
+int start,count;
+if(lines<K){
+  start=0;
+  count=lines;
+}else{
+  start=lines%K;
+  count=K;
+}
+for(int i=0; i<count;++i){
+  count<<L[(start+i)%K]<<end1;
+}
