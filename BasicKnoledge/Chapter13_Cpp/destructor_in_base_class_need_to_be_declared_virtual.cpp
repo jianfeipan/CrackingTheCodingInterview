be3e7a6f200e2ed0f713c258de14141e7c1cf2f7
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-12 17:34:26 +0100

    Update destructor_in_base_class_need_to_be_declared_virtual.cpp

--- BasicKnoledge/Chapter13_Cpp/destructor_in_base_class_need_to_be_declared_virtual.cpp
+++ BasicKnoledge/Chapter13_Cpp/destructor_in_base_class_need_to_be_declared_virtual.cpp
@@ -6,3 +6,40 @@ A virtual destructor works in the same way. A destructor gets called when an obj
 When any derived class object goes out of scope, the destructor of that derived class gets called first. It then calls its parent class destructor so memory allocated to the object is properly released.
 But, if we call delete on a base pointer which points to a derived class object, the base class destructor gets called first (for non-virtual function). For example:
 */
+class Base{
+  public: 
+    Base(){cout<<"Base Constructor"<<endl;}
+    ~Base(){cout<<"Base Destructor"<<endl;}
+}
+class Derived(): public Base{
+  public::
+    Derived(){cout<<"Derived Constructor"<<endl}
+    ~Derived(){cout<<"Derived Destructor"<<endl}
+}
+
+void mai(){
+  Base *p=new Derived();
+  delete p;
+}
+/*output:
+Base Constructor
+Derived Constructor
+Base Destructor
+*/
+
+
+/*If we declare the base class destructor as virtual, this makes all the derived class destructors virtual as well.
+If we replace the above destructor with:*/
+class Base{
+  public: 
+    Base(){cout<<"Base Constructor"<<endl;}
+    vitrual ~Base(){cout<<"Base Destructor"<<endl;}
+}
+
+/*output:
+Base Constructor
+Derived Constructor
+Derived Destructor
+Base Destructor
+So we should use virtual destructors if we call delete on a base class pointer which points to a derived class.
+*/
