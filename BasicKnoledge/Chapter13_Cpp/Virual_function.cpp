c0f938bef39a54323a3b83b8c6bf8af64ed74d2e
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-12 16:39:46 +0100

    Update Virual_function.cpp

--- BasicKnoledge/Chapter13_Cpp/Virual_function.cpp
+++ BasicKnoledge/Chapter13_Cpp/Virual_function.cpp
@@ -1,4 +1,22 @@
 class Sharpe{
   public:
-    
+    int edge_length;
+    virtual int circumference(){
+      cout << "Circumference of Basic Class\n";
+      return 0;
+    }
+};
+
+class Triangle:public Shape{
+  public:
+    int circumference(){
+      cout << "Circumference of Triangle Class\n";
+      return 3*edge_length;
+    }
+};
+void main(){
+  Shape *x=new Shape();
+  x->circumference();// prints “Circumference of Base Class”
+  Shape *y=new Tirangle();
+  y->circumference();// prints “Circumference of Triangle Class”
 }
