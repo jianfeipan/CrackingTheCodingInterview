5f37c6c6e7c3eaf338f1468b9e67db5f5482bff3
jianfeipan <jianfei.pan.tb@gmail.com>
2015-01-12 17:09:19 +0100

    Update NameHidingInCPP.cpp

--- BasicKnoledge/Chapter13_Cpp/NameHidingInCPP.cpp
+++ BasicKnoledge/Chapter13_Cpp/NameHidingInCPP.cpp
@@ -1,1 +1,25 @@
-class FirstClass{}
+class FirstClass{
+  public:
+    virtual void MethodA(int);
+    virtual void MethodA(int, int);
+};
+void FirstClass::MethodA(int i){
+  std::cout <<"ONE!\n";
+}
+void FirstClass::MethodA(int i, int i){
+  std::cout <<"TWO!!\n";
+}
+
+class SecondClass{
+  public: 
+    void MethodA(int);
+};
+void SecondClass::MethodA(int i){
+  std::cout << "Tree!!!\n";
+}
+
+void main(){
+  SecondClass a;
+  a.MethodA(1);
+  a.MethodA(1,1);/*However, the second call won’t work, since the two-parameter MethodA is not visible. That is name hiding.*/
+}
